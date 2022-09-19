90fafde5e7acd1616f150996c94003c8e9ff3369
jignesh2413 <113872147+jignesh2413@users.noreply.github.com>
2022-09-19 08:47:23 +0530

    Daily Task

---
+++ Assignment/task1.py
@@ -0,0 +1,20 @@
+month = 24
+years = 12
+print("No of years are" , month/years)
+
+month = 12
+years = 5
+print("No of Months are", month*years)
+
+p = 1000
+r = 0.1
+n = 5
+
+print(f"SI = {p}*{r}*{n} / {100} = {p*r*n/100}" )
+
+Hours = 60
+Days = 24
+Minutes = 60
+print("No of days are", Hours/Days)
+print("No of Minutes are", Hours*Minutes)
+
